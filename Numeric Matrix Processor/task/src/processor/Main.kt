13ab0fb8c15089dd87c7ae18a8f35dac88ddaa4b
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-08-17 00:16:01 +0900

    Stage 3/6: Matrix by matrix multiplication

--- Numeric Matrix Processor/task/src/processor/Main.kt
+++ Numeric Matrix Processor/task/src/processor/Main.kt
@@ -3,24 +3,47 @@ package processor
 import java.util.*
 
 fun main() {
-    val scanner = Scanner(System.`in`)
-//    testSum(scanner)
-    testMulByNum(scanner)
+    printMenu()
+    while (true) {
+        when (readLine()!!) {
+            "1" -> testSum()
+            "2" -> testMulByNum()
+            "3" -> testMulByMatrix()
+            "0" -> break
+        }
+    }
 }
 
-fun testMulByNum(scanner: Scanner) {
-    val a = Matrix.read(scanner)
-    val c = scanner.nextInt()
+fun printMenu() {
+    println("1. Add matrices\n" +
+            "2. Multiply matrix by a constant\n" +
+            "3. Multiply matrices\n" +
+            "0. Exit")
+}
+
+fun testMulByMatrix() {
+    val a = Matrix.read()
+    val b = Matrix.read()
+    try {
+        println(a.mul(b))
+    } catch (e: IllegalArgumentException) {
+        println("ERROR: ${e.message}")
+    }
+}
+
+fun testMulByNum() {
+    val a = Matrix.read()
+    val c = readLine()!!.toInt()
     System.err.println(c)
     println(a.mul(c))
 }
 
-private fun testSum(scanner: Scanner) {
-    val a = Matrix.read(scanner)
-    val b = Matrix.read(scanner)
+private fun testSum() {
+    val a = Matrix.read()
+    val b = Matrix.read()
     try {
         println(a.add(b))
     } catch (e: IllegalArgumentException) {
-        println("ERROR")
+        println("ERROR: ${e.message}")
     }
 }
--- Numeric Matrix Processor/task/src/processor/Matrix.kt
+++ Numeric Matrix Processor/task/src/processor/Matrix.kt
@@ -4,19 +4,21 @@ import java.lang.IllegalArgumentException
 import java.util.*
 
 data class Matrix(val n: Int, val m: Int) {
-    private val matrix = Array(n) { Array(m) { 0 } }
+    private val matrix = Array(n) { Array(m) { 0.0 } }
 
     companion object {
-        fun read(scanner: Scanner): Matrix {
+        fun read(): Matrix {
+            val scanner = Scanner(System.`in`)
             val n = scanner.nextInt()
             val m = scanner.nextInt()
             val result = Matrix(n, m)
             for (i in 0 until n) {
                 for (j in 0 until m) {
-                    result.matrix[i][j] = scanner.nextInt()
+                    result.matrix[i][j] = scanner.nextDouble()
                 }
             }
             System.err.println(result)
+            scanner.close()
             return result
         }
     }
@@ -45,6 +47,25 @@ data class Matrix(val n: Int, val m: Int) {
         for (i in 0 until n)
             for (j in 0 until m)
                 result.matrix[i][j] = this.matrix[i][j] * c
+        return result
+    }
+
+    fun mul(other: Matrix): Matrix {
+        if (this.n != other.m && this.m != other.n) {
+            throw IllegalArgumentException("Dimensions are not suitable ${this.n}x${this.m} + ${other.n}x${other.m}")
+        }
+        val result = Matrix(this.n, other.m)
+        for (i in 0 until n) {
+            for (j in 0 until other.m) {
+                var sum = 0.0
+                for (k in 0 until m) {
+                    sum += this.matrix[i][k] * other.matrix[k][j]
+//                    System.err.println("[$i, $k](${this.matrix[i][k]}) * [$k, $j](${other.matrix[k][j]}) = $sum")
+                }
+                result.matrix[i][j] = sum
+//                System.err.println("[$i, $j] = $sum")
+            }
+        }
         return result
     }
 }
